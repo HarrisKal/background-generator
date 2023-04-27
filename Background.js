3641e8ba09101f92b58d073aef17eca2e31e0acb
HarrisKal <chtsiaousidis79@gmail.com>
2023-04-27 22:05:26 +0300

    SetGradient replaced with string concatination with string interpolation

--- Background.js
+++ Background.js
@@ -4,11 +4,8 @@ let input2 = document.querySelector('.color2');
 let body = document.querySelector('body');
 
 function setGradient() {
-    body.style.background = 
-    "linear-gradient(to right, " + 
-    input1.value + ", " + 
-    input2.value +")";
-    h3.textContent = body.style.background + ";";
+    body.style.background = `linear-gradient(to right, ${input1.value}, ${input2.value})`
+    h3.textContent = `${body.style.background};`
 }
 
 input1.addEventListener("input", setGradient);
